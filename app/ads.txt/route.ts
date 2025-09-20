23bc367d273fe3c282c46d9feb1396a58bb086e5
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-09-20 10:41:23 +0530

    google adsense

--- app/ads.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.redirect(
-    "https://srv.adstxtmanager.com/19390/rolespot.space",
-    301
-  );
-}
