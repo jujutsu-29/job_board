bdfd1b3ea481999c04a4039fd97285992931630e
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-11-05 22:13:59 +0530

    toast added and loading screen on admin ai page

--- app/admin/jobsai/page.tsx
+++ app/admin/jobsai/page.tsx
@@ -1,24 +1,43 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
 import { createJob } from "@/lib/server/jobs";
 import { redirect } from "next/navigation";
-
-const handleSubmit = async (e: React.FormEvent) => {
-  e.preventDefault();
-  const form = e.target as HTMLFormElement;
-  const url = (form.elements.namedItem("url") as HTMLInputElement).value;
-
-  const res = await createJob(url, true);
-
-  if(res.success){
-    redirect(`/admin/jobs/`);
-  }
-  // console.log("Job creation response:", res);
-}
+import { useState } from "react";
 
 export default function RandomPage() {
+
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const url = (form.elements.namedItem("url") as HTMLInputElement).value;
+
+    const res = await createJob(url, true);
+
+    if (res.success) {
+      toast({
+        title: "Job Created",
+        description: `Redirecting to admin page.`,
+        variant: "default",        
+      });
+    } else {
+      toast({
+        title: "Error",
+        description: res.error || "An unknown error occurred.",
+        variant: "destructive",
+      });
+
+    }
+    setLoading(false);
+    if (res.success) {
+      redirect(`/admin/jobs/`);
+    }
+  };
+
   return (
     <div className="flex min-h-[calc(100vh-8rem)] flex-col items-center justify-center bg-background px-4 py-12 text-center">
       <div className="max-w-md space-y-6">
@@ -32,8 +51,15 @@ export default function RandomPage() {
           onSubmit={handleSubmit}
           className="flex flex-col sm:flex-row w-full max-w-sm items-center space-y-2 sm:space-y-0 sm:space-x-2 mx-auto"
         >
-          <Input name="url" type="url" placeholder="https://example.com" className="flex-grow" />
-          <Button type="submit" className="w-full sm:w-auto">Submit</Button>
+          <Input
+            name="url"
+            type="url"
+            placeholder="https://example.com"
+            className="flex-grow"
+          />
+          <Button type="submit" className="w-full sm:w-auto">
+            {loading ? "Creating Job..." : "Submit"}
+          </Button>
         </form>
       </div>
     </div>
